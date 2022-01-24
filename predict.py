d3159734867566167caff71099825793f90d541c
iamvigneshwars <iamvigneshwars@gmail.com>
2022-01-24 20:11:01 +0000

    README

--- predict.py
+++ predict.py
@@ -1,13 +1,13 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-from model import transformer
+from model import hybridModel
 import tensorflow as tf
 from tensorflow.keras import layers
-from preprocess import create_data
+from preprocess import createData
 
 def classify(data, model):
     classes = ["BACKGROUND", "CONCLUSIONS", "METHODS", "OBJECTIVE", "RESULTS"]
-    data = create_data(data)
+    data = createData(data)
     abs_pred_probs = model.predict(x = data)
     abs_preds = tf.argmax(abs_pred_probs, axis=1)
     abs_pred_classes = [classes[i] for i in abs_preds]
@@ -23,30 +23,20 @@ def classify(data, model):
 
     return results
 
-
-# model = transformer()
-# cont = 'y'
-# while cont == 'y':
-#     abstract = input("\nEnter the Abstract: \n\n")
-#     result = classify(abstract, model)
-    
-#     for r in result:
-#         print(r['label'], " : ", r['sentence'], "\n")
-
 if __name__ == "__main__":
-    model= transformer()
+    model= hybridModel()
     try:
         cont = 'y'
-        while cont == 'y':
+        while cont == 'y' or cont =='':
             abstract = input("\nEnter the Abstract: \n\n")
             result = classify(abstract, model)
             
             for r in result:
                 print(r['label'], " : ", r['sentence'], "\n")
 
-            cont = str(input("\nWant to skim another unstructured abstract? [y/n] : ").lower())
+            cont = str(input("\nWant to skim another unstructured abstract? [Y/n] : ").lower())
 
     except:
 
-        print("Error !")
+        print("Please Enter only unstructured medical abstracts with atleat 5 lines!")
 
